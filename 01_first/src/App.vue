c8ddc1e0a18c829cf5ead96e3accea4de2c0d021
ismoon <insu4341@gmail.com>
2024-08-13 17:41:55 +0900

    20240813_8교시

--- 01_first/src/App.vue
+++ 01_first/src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <main>
     <h2>Vue.js 기초</h2>
-    <Directive1/>
-    <Directive2/>
-    <Options/>
+    <!-- <Directive1/> -->
+    <!-- <Directive2/> -->
+    <!-- <Options/> -->
+    <!-- <Events/> -->
+    <StyleTest/>
+    <StyleTest2/>
   </main>
 </template>
 
@@ -11,13 +14,19 @@
   import Directive1 from './components/Directive1.vue';
   import Directive2 from './components/Directive2.vue';
   import Options from './components/Options.vue';
+  import Events from './components/Events.vue';
+  import StyleTest from './components/StyleTest.vue';
+  import StyleTest2 from './components/StyleTest2.vue';
 
   export default {
     name: 'App',
     components: {
       Directive1, // Directive1: Directive1
       Directive2,
-      Options
+      Options,
+      Events,
+      StyleTest,
+      StyleTest2
     }
   }
 </script>
