93f7cf0887251b8576218c98ba5286e024aec73b
ismoon <insu4341@gmail.com>
2024-08-19 17:39:45 +0900

    20240819_8교시

--- 04_university/src/components/Header.vue
+++ 04_university/src/components/Header.vue
@@ -1,5 +1,6 @@
 <template>
     <header>
+        <!-- 
         <ul class="nav">
             <li class="nav-item">
                 <RouterLink class="nav-link" to="/">HOME</RouterLink>
@@ -7,6 +8,23 @@
             <li class="nav-item">
                 <RouterLink class="nav-link" to="/departments">학과 목록 조회</RouterLink>
             </li>
+        </ul> 
+        -->
+        <!--  
+        명명된 라우터의 경우 v-bind를 이용해서 to 속성에 문자열이 아닌 객체를 바인딩해야 한다.
+        -->
+        <ul class="nav">
+            <li class="nav-item">
+                <RouterLink class="nav-link" :to="{name: 'home'}">HOME</RouterLink>
+            </li>
+            <li class="nav-item">
+                <RouterLink class="nav-link" :to="{name: 'departments'}">학과 목록 조회</RouterLink>
+            </li>
+            <!-- 
+            <li class="nav-item">
+                <RouterLink class="nav-link" :to="{name: 'departments/no', params: {no: '033'}, query: {name: '홍길동', age: 34}}">학과 상세 조회</RouterLink>
+            </li> 
+            -->
         </ul>
     </header>
 </template>
--- 04_university/src/pages/Departments.vue
+++ 04_university/src/pages/Departments.vue
@@ -1,11 +1,52 @@
 <template>
     <div>
         <h2>학과 목록 조회</h2>
+
+        <DepartmentList :departments="departments"/>
+        <PageNation/>
     </div>
 </template>
 
 <script setup>
+    import DepartmentList from '@/components/DepartmentList.vue';
+    import PageNation from '@/components/PageNation.vue';
+    import apiClient from '@/api';
+    import { onMounted, ref } from 'vue';
+    import { useRoute } from 'vue-router';
 
+    const departments = ref([]);
+    const currentRoute = useRoute();
+    const currentPage = ref(parseInt(currentRoute.query.page) || 1);
+
+
+    // axios 사용 방법
+    // const fetchDepartments = () => {
+    //     apiClient.get('/departments?page=1&numOfRows=10')
+    //             .then((response) => {
+    //                 console.log(response);
+    //             })
+    //             .catch((error) => {
+    //                 console.log(error);
+    //             });
+    // };
+
+    // async / await
+    const fetchDepartments = async (page) => {
+        try {
+            const response = await apiClient.get(`/departments?page=${page}&numOfRows=10`);
+
+            console.log(response);
+
+            departments.value = response.data.result;
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    onMounted(() => {
+        fetchDepartments(currentPage.value);
+    });
 </script>
 
 <style scoped>
