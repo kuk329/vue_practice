69ceaddc5cff77e11917207cd4277c6cda072737
kuk329 <kuk329@cau.ac.kr>
2024-08-20 14:05:38 +0900

    학과 등록 api

--- 04_university/src/components/DepartmentForm.vue
+++ 04_university/src/components/DepartmentForm.vue
@@ -1,26 +1,26 @@
 <template>
     <div>
-        <form>
+        <form @submit.prevent="emit('form-submit',formData)">
             <div class="mb-3">
                 <label for="name" class="form-label">학과 이름</label>
-                <input type="text" class="form-control" id="name">
+                <input type="text" class="form-control" id="name" v-model="formData.name">
 
             </div>
             <div class="mb-3">
                 <label for="category" class="form-label">계열</label>
-                <input type="text" class="form-control" id="category">
+                <input type="text" class="form-control" id="category" v-model="formData.category">
             </div>
 
             <div class="mb-3">
                 <label for="openYn" class="form-label">개설 여부</label>
-                <select class="form-select" >
+                <select class="form-select" id="openYn" v-model="formData.openYn">
                     <option value="Y">예(Y)</option>
                     <option value="N">아니오(N)</option>
                 </select>
             </div>
             <div class="mb-3">
                 <label for="capacity" class="form-label">정원</label>
-                <input type="number" class="form-control" id="capacity">
+                <input type="number" class="form-control" id="capacity" v-model="formData.capacity">
             </div>
             <button type="submit" class="btn btn-primary">{{submitButtonText }}</button>
         </form>
@@ -36,12 +36,17 @@ import {ref,watch} from 'vue';
 });
 
 const formData = ref({});
+const emit = defineEmits(['form-submit']);
+
+const submitClick = ()=>{
+    emit('form-submit',formData.value);
+}
 
 watch(props.initFormData,
     (newFormData)=>{
         formData.value = {...newFormData}; // 구조 분해 할당
     },
-    {immediate:true}
+    {immediate:true} // 관찰속성 등록하고 바로 한번 실행함.
 )
 
 </script>
--- 04_university/src/pages/AddDepartment.vue
+++ 04_university/src/pages/AddDepartment.vue
@@ -1,20 +1,41 @@
 <template>
     <div>
         <h2>학과 등록</h2>
-        <DepartmentForm submitButtonText="학과 등록" :initFormData="formData"/>
+        <DepartmentForm submitButtonText="학과 등록" :initFormData="initFormData"
+            @form-submit="formSubmit"/>
     </div>
 </template>
 
 <script setup>
     import DepartmentForm from '@/components/DepartmentForm.vue';
     import {ref} from 'vue';
+    import apiClient from '@/api';
+    import {useRoute, useRouter} from 'vue-router';
 
-    const formData = ref({
+    const router = useRouter();
+
+    const initFormData = ref({
         name:'',
         category:'',
-        openYn:'Y',
+        openYn:'N',
         capacity:30
     });
+
+    const formSubmit = async(formData)=>{
+        //console.log(formData);
+
+        try{
+            const response  = await apiClient.post('/departments',formData);
+            if(response.data.code===201){
+                alert("정상적으로 등록되었습니다.");
+
+                router.push({name:"departments"});
+            }
+
+        }catch(err){
+            console.log(err);
+        }
+    }
 
 </script>
    
